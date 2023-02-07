37e818dced789a34343348d0fafcbc5fa594a661
Kevin Zanao <kevinzanao2108@gmail.com>
2023-02-07 15:32:27 -0300

    responsividade ajustada

--- consult-list.js
+++ consult-list.js
@@ -26,6 +26,33 @@ export const consultList = {
             timeStart: "17:20",
             duration: "40",
          }
+      },
+      {
+         consult: {
+            image: "./assets/img/image2.png",
+            name: "Luciana Dias",
+            type: "remota",
+            timeStart: "14:00",
+            duration: "01:00",
+         },
+      },
+      {
+         consult: {
+            image: "./assets/img/image4.png",
+            name: "Marcos Correia",
+            type: "remota",
+            timeStart: "17:20",
+            duration: "40",
+         }
+      },
+      {
+         consult: {
+            image: "./assets/img/image5.png",
+            name: "Clara Lemos",
+            type: "local",
+            timeStart: "18:00",
+            duration: "01:00",
+         },
       }
    ],
    tomorrow: [
@@ -102,6 +129,24 @@ export const consultList = {
             timeStart: "17:20",
             duration: "40",
          }
-      }
+      },
+      {
+         consult: {
+            image: "./assets/img/image2.png",
+            name: "Luciana Dias",
+            type: "remota",
+            timeStart: "14:00",
+            duration: "01:00",
+         },
+      },
+      {
+         consult: {
+            image: "./assets/img/image5.png",
+            name: "Clara Lemos",
+            type: "local",
+            timeStart: "18:00",
+            duration: "01:00",
+         },
+      },
    ]
 }
